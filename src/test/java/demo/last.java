b9b623d50ae08a61ef0a9e31b6876aed5a70748c
prajwal <prajwaldkg@gmail.com>
2026-01-17 10:43:20 +0530

    hello

--- src/test/java/demo/last.java
+++ src/test/java/demo/last.java
@@ -7,8 +7,9 @@ public class last {
 	public void dog() {
 		
 		System.out.println("dog barks");
-		System.out.println("bow bow");
+		System.out.println("hihihi");
 		System.out.println("yy");
 		System.out.println("qwerty");
+		System.out.println("bye");
 	}
 }
